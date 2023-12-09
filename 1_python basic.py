29cd02d1a326c55f14e1f9761ca446651fb7d562
anand-a11 <79590006+anand-a11@users.noreply.github.com>
2023-12-09 11:19:34 +0530

    Python Basics coding
    Python Basics coding

---
+++ 1_python basic.py
@@ -0,0 +1,486 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+#this is my first prog
+print('this is my first python program')
+
+
+# In[5]:
+
+
+print " this is my first program"
+
+
+# In[159]:
+
+
+#my name is sudh 
+"""FAFASFSAFSAFASFASFASFSDFASF
+ASDFASF
+ASDFASDF
+"""
+a 
+
+
+# In[14]:
+
+
+"""print("fsdfsdfs")
+print("Fsdfsfs\FSDFSAFASFASDDF)
+print("sdfsfsdf")"""
+
+
+# In[21]:
+
+
+afsdf = 10 
+
+
+# In[22]:
+
+
+afsdf
+
+
+# In[20]:
+
+
+type(a)
+
+
+# In[28]:
+
+
+b = '34554sudh 45345fgf sgsd sdfg sh sghf h dhdfh df'
+
+
+# In[29]:
+
+
+b
+
+
+# In[30]:
+
+
+type(b)
+
+
+# In[33]:
+
+
+print(b , a ,"Fsdfsfsd")
+
+
+# In[34]:
+
+
+a = 45
+b = "sudh "
+
+
+# In[36]:
+
+
+a, b  , s = 45 , "sudh" , "this is my name "
+
+
+# In[37]:
+
+
+a 
+
+
+# In[38]:
+
+
+b
+
+
+# In[39]:
+
+
+s 
+
+
+# In[40]:
+
+
+type(b)
+
+
+# In[42]:
+
+
+type(s)
+
+
+# In[43]:
+
+
+type(a)
+
+
+# In[45]:
+
+
+print(a,b,s)
+
+
+# In[47]:
+
+
+b  = '"fsdf"'
+
+
+# In[48]:
+
+
+b 
+
+
+# In[49]:
+
+
+1b = "fsfsd"
+
+
+# In[50]:
+
+
+B = "fsdf"
+
+
+# In[51]:
+
+
+B
+
+
+# In[52]:
+
+
+b = "fsfsd"
+
+
+# In[53]:
+
+
+2b = "sdfs"
+
+
+# In[57]:
+
+
+_b = 234534
+
+
+# In[58]:
+
+
+_b
+
+
+# In[59]:
+
+
+b& = "fsds"
+
+
+# In[60]:
+
+
+name = "sudh"
+userid = "IN001"
+password = "fsfsfsd"
+mail = "sudhans@ienru.ai"
+
+
+# In[63]:
+
+
+a = 1.6423424242243434234234
+
+
+# In[64]:
+
+
+type(a )
+
+
+# In[70]:
+
+
+b = True 
+c = False 
+
+
+# In[66]:
+
+
+type(b)
+
+
+# In[68]:
+
+
+type(c)
+
+
+# In[71]:
+
+
+print(True + True + True + False + True  )
+print(False - True+ True ) 
+
+
+# In[73]:
+
+
+True + True -False
+
+
+# In[75]:
+
+
+True / False
+
+
+# In[86]:
+
+
+n  = 45 + 6j
+m = 56 + 8j
+
+
+# In[89]:
+
+
+type(m)
+
+
+# In[87]:
+
+
+n* m
+
+
+# In[80]:
+
+
+n.real
+
+
+# In[81]:
+
+
+n.imag
+
+
+# In[88]:
+
+
+b = "sdf"
+type(b)
+
+
+# In[94]:
+
+
+"1"+ "sudh"
+
+
+# In[96]:
+
+
+1+ 1.0
+
+
+# In[97]:
+
+
+"sudh" + "sudh" + "sudh"
+
+
+# In[100]:
+
+
+"sudh" * 3
+
+
+# In[102]:
+
+
+int('23')
+
+
+# In[104]:
+
+
+int("2354dfsdfs")
+
+
+# In[105]:
+
+
+a = 10 
+
+
+# In[112]:
+
+
+a = input("enter a interger ")
+
+
+# In[113]:
+
+
+type(a)
+
+
+# In[131]:
+
+
+a = int(input("this is my var a "))
+b = int(input("this is my var b "))
+c = int(input("this is my var c "))
+o = a+b/c
+o1 = o + 78 + 6j
+str(o1) + "sudh"
+
+
+# In[124]:
+
+
+o = a+b/c
+o1 = o + 78 + 6j
+
+
+# In[130]:
+
+
+o1
+
+
+# In[129]:
+
+
+str(o1) + "sudh"
+
+
+# In[134]:
+
+
+a = int(input("input a number lesser then 34 -- "))
+
+
+# In[158]:
+
+
+if a <34 :
+    pass
+
+
+# In[150]:
+
+
+a = int(input("input a number"))
+if a <34:
+    print("data input is lesser then 34 and i am fine with it ")
+elif a < 100:
+    print(" my data is lesser then 100")
+    
+else:
+    print ( "user input is not lesser then 34")
+
+
+# In[155]:
+
+
+salary = int(input("enter your salary"))
+if( salary > 1000):
+    print("you salary is good")
+    if salary > 5000:
+        print("i will go with a card nano")
+        if True:
+            print("false code")
+    elif salary >10000:
+        print("i will go with maruti")
+    elif salary > 50000:
+        print("i wil go with toyota")
+    else :
+        print("i will rent a car ")
+elif salary > 500:
+    print(" i will go for bike ")
+elif salary> 100:
+    print("i will not buy anyting")
+else :
+    print("i will go for savinig ")
+
+
+# In[156]:
+
+
+salary > 5000
+
+
+# In[ ]:
+
+
+void test(int i , float j ){
+    
+}
+
+
+# In[177]:
+
+
+name = "sudh"
+test = "dfsfsf"
+
+
+# In[182]:
+
+
+def test(name = str  , test = int):
+    name = "fsfds"
+    test= "fsdfs"
+    print(name, test)
+
+
+# In[183]:
+
+
+test()
+
+
+# In[197]:
+
+
+def test1(name , id1):
+    
+    print(test(),name , id1)
+    
+
+
+# In[198]:
+
+
+test1("Sds","ds")
+
+
+# In[196]:
+
+
+def test():
+        return "dasas"
+
+
+# In[ ]:
+
+
+
+
